c394caa1df712464e5c63c6820ec58db3babade3
Bradley Huang <ultravenusk1@gmail.com>
2022-09-22 12:24:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CLIENT_ID = os.getenv('CLIENT_ID'),
-CLIENT_SECRET = os.getenv('CLIENT_SECRET'),
-PASSWORD = os.getenv('PASSWORD'),
-USER_AGENT = os.getenv('USER_AGENT'),
-USERNAME = os.getenv('USERNAME'),
-
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+PASSWORD = os.getenv('PASSWORD')
+USER_AGENT = os.getenv('USER_AGENT')
+OATHUSER = os.getenv('OATHUSER')
 
 reddit = praw.Reddit(
     client_id = CLIENT_ID,
     client_secret = CLIENT_SECRET,
     password= PASSWORD,
     user_agent = USER_AGENT,
-    username = USERNAME,
+    username = OATHUSER,
 )
 
-if reddit.read_only:
-    print('Failed to login, check your credentials')
-    sys.exit()
-else:
-    print('Logged in successfully')
+subreddit = reddit.subreddit("askreddit")
 
+for submission in subreddit.top(limit=25):
+    print(submission.title)
