5049bdb3010c018e9734241d60da4dbe34b8e943
fruitrobo <mathew.pan2@gmail.com>
2022-09-23 22:15:29 -0400

    Update tts.py
    Now it converts the array of comments to an audio file and saves it.

--- TextToSpeech/tts.py
+++ TextToSpeech/tts.py
@@ -1,10 +1,14 @@
 import pyttsx3
+import sys 
+
+sys.path.append("..")
+import main
 
 audio = pyttsx3.init()
 audio.setProperty('rate', 200)
 audio.setProperty('volume', 0.9)
 
-postTitle = main.postTitle
+#postTitle = main.postTitle
 
-audio.save_to_file(postTitle, 'textToSpeechAudio.mp3')
+audio.save_to_file(main.comments, 'textToSpeechAudio.mp3')
 audio.runAndWait()
