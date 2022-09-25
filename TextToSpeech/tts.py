46f4f254f80835d9c3075139d9738074a791f043
Bradley Huang <ultravenusk1@gmail.com>
2022-09-24 23:22:42 -0400

    Update tts.py

--- TextToSpeech/tts.py
+++ TextToSpeech/tts.py
@@ -1,14 +1,12 @@
 import pyttsx3
 import sys 
+import main
 
 sys.path.append("..")
-import main
 
 audio = pyttsx3.init()
 audio.setProperty('rate', 200)
 audio.setProperty('volume', 0.9)
-
-#postTitle = main.postTitle
 
 audio.save_to_file(main.comments, 'textToSpeechAudio.mp3')
 audio.runAndWait()
