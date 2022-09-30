e85a01f275f3c61e9a6d12eb709d57366c9d99a5
fruitrobo <mathew.pan2@gmail.com>
2022-09-29 20:41:33 -0400

    Update upload_video.py
    Added a upload video function

--- Upload/Youtube/upload_video.py
+++ Upload/Youtube/upload_video.py
@@ -86,18 +86,18 @@ def get_authenticated_service(args):
 
 def initialize_upload(youtube, options):
   tags = None
-  if options.keywords:
-    tags = options.keywords.split(",")
+  if options['keywords']:
+    tags = options['keywords'].split(",")
 
   body=dict(
     snippet=dict(
-      title=options.title,
-      description=options.description,
+      title=options['title'],
+      description=options['description'],
       tags=tags,
-      categoryId=options.category
+      categoryId=options['category']
     ),
     status=dict(
-      privacyStatus=options.privacyStatus
+      privacyStatus=options['privacyStatus']
     )
   )
 
@@ -116,7 +116,7 @@ def initialize_upload(youtube, options):
     # practice, but if you're using Python older than 2.6 or if you're
     # running on App Engine, you should set the chunksize to something like
     # 1024 * 1024 (1 megabyte).
-    media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
+    media_body=MediaFileUpload(options['file'], chunksize=-1, resumable=True)
   )
 
   resumable_upload(insert_request)
@@ -156,23 +156,29 @@ def resumable_upload(insert_request):
       print("Sleeping %f seconds and then retrying..." % sleep_seconds)
       time.sleep(sleep_seconds)
 
-if __name__ == '__main__':
-  argparser.add_argument("--file", required=True, help="Video file to upload")
-  argparser.add_argument("--title", help="Video title", default="Test Title")
-  argparser.add_argument("--description", help="Video description", default="Test Description")
-  argparser.add_argument("--category", default="22",help="Numeric video category. " + "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
-  argparser.add_argument("--keywords", help="Video keywords, comma separated",default="")
-  argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
-  args = argparser.parse_args()
 
-  if not os.path.exists(args.file):
+def upload_Video(video_args):
+  args = argparser.parse_args()
+  if not os.path.exists(video_args['file']):
     exit("Please specify a valid file using the --file= parameter.")
 
   youtube = get_authenticated_service(args)
   try:
-    initialize_upload(youtube, args)
+    initialize_upload(youtube, video_args)
   except HttpError as e:
     print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
 
 
 
+temp = "reddit stuff"
+video_args = {
+    "file": "test.mp4",
+    "title": "Today in r/AskReddit: " + temp ,
+    "description": "placeholder ",
+    "category": "22",
+    "keywords": "reddit, askreddit, idk ", 
+    "privacyStatus": "private"
+}
+
+
+print(video_args['title'])
